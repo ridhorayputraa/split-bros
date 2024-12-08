c18a9c0e4c9c86d3e41cf094abb879811f2b153d
wahyualfaris <abidkhai007@gmail.com>
2024-12-08 20:56:53 +0700

    splashscreenwahyu

--- src/page/SplashScreen.jsx
+++ src/page/SplashScreen.jsx
@@ -4,8 +4,19 @@ import gambarRumah from "../../public/gambar-rumah.png";
 function SplashScreen() {
   return (
     <>
-      <div>SplashScreen</div>
+      <div style={{display: "flex", justifyContent: "center"}}>
+        <h3 style={{fontSize: "15px"}}>Welcome To</h3>
+        <h1 style={{fontSize: "25px"}}>SplitBros</h1>
+      </div>
+      <div style={{marginTop: "20px"}}>SplashScreen</div>
       <img src={gambarRumah} alt="" />
+
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        <label htmlFor="merchant">Let me Know your name</label>
+        <input id="merchant"/>
+      </div>
+      <button style={{ marginTop: "16px", width: "50px", color: "green" }}>Submit</button>
+      
     </>
   );
 }
