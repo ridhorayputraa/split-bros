6de171db99dfaa72b5787163a8e24e0872905e7a
Vicky Albaihaqi <rifqialfarez2906@gmail.com>
2024-12-09 12:17:00 +0700

    ubah split

--- src/page/Split.jsx
+++ src/page/Split.jsx
@@ -6,17 +6,54 @@ function Split() {
       <button>back</button>
       <div style={{ display: "flex", flexDirection: "column" }}>
         <label htmlFor="merchant">Let me Know what you buy!</label>
-        <input id="merchant" placeholder="Mixue" />
+        <input 
+          readOnly
+          style={{ height: "25px" }}
+          id="merchant"/>
       </div>
       <div style={{ display: "flex", flexDirection: "column" }}>
-        <label htmlFor="merchant">Let me Know what you buy!</label>
+        <label htmlFor="merchant">How Many Bros </label>
+        <input
+          readOnly
+          style={{ height: "25px" }}
+          id="merchant" />
+      </div>
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        <label htmlFor="merchant">Bill your bros 1 </label>
+        <input
+          readOnly
+          style={{ height: "25px" }}
+          id="merchant" />
+      </div>
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        <label htmlFor="merchant">Bill your bros 2 </label>
+        <input
+          readOnly
+          style={{ height: "25px" }}
+          id="merchant" />
+      </div>
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        <label htmlFor="merchant">Bill your bros 3 </label>
+        <input
+          readOnly
+          style={{ height: "25px" }}
+          id="merchant" />
+      </div>
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        <label htmlFor="merchant">Bill your bros 4 </label>
+        <input
+          readOnly
+          style={{ height: "25px" }}
+          id="merchant" />
+      </div>
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        <label htmlFor="merchant">Total bill </label>
         <input
           readOnly
           style={{ height: "128px" }}
-          id="merchant"
-          placeholder="Rp 12.000"
-        />
+          id="merchant" />
       </div>
+    <button>Submit</button>
     </>
   );
 }
