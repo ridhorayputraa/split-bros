14355d30c34b1db342fe786700002065a6ccc71f
kaff167 <kaffasyahida3@gmail.com>
2024-12-09 19:32:28 +0700

    MainKaffa

--- src/page/MainKaffa.jsx
+++ src/page/MainKaffa.jsx
@@ -4,14 +4,18 @@ function MainKaffa() {
   return (
     <>
       <div>
-        <p>Hello, Kaffa</p>
+        <p>Hello, nadia</p>
         <h3>Split your bill with Bros</h3>
       </div>
-      <div style={{ backgroundColor: "black", padding: "1rem", width: "68px" }}>
-        <p>Manuk</p>
+      <div style={{ backgroundColor: "black", padding: "1rem", width: "240px" }}>
+        <p>Today Spending (personal)</p> <br />
+        <p>Rp 20.000</p>
       </div>
 
-      <button style={{ marginTop: "16px" }}>Lets split</button>
+      <button style={{ marginTop: "16px" }}>Lets split!</button>
+      <div>
+      <p>History (24 Hours)</p>
+      </div>
     </>
   );
 }
