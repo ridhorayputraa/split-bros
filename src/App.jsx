3f99d9596c8ad32cbb397aeaa8deba18e21df924
Vicky Albaihaqi <rifqialfarez2906@gmail.com>
2024-12-09 12:49:13 +0700

    Split

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,13 @@ import "./App.css";
 import Split from "./page/Split";
 import SplashScreen from "./page/SplashScreen";
 import MainNadia from "./page/MainNadia";
-import MainKaffa from "./page/MainKaffa";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <Split/>
+      <MainKaffa />
     </>
   );
 }
